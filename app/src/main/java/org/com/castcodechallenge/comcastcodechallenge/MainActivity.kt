09a025422fe1325e7a5c0719cb7acba0b2f921f2
Jonathan Cruz <jitzian@gmail.com>
2019-01-25 17:45:09 -0600

    cc jc -Dummy injection for testing rest API fetching

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/MainActivity.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/MainActivity.kt
@@ -7,17 +7,70 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.com.castcodechallenge.comcastcodechallenge.api.RestApi
+import org.com.castcodechallenge.comcastcodechallenge.api.model.CharactersResult
+import org.com.castcodechallenge.comcastcodechallenge.constants.GlobalConstants.Companion.format
+import org.com.castcodechallenge.comcastcodechallenge.dependency.injection.components.DaggerViewModelInjector
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import java.util.logging.Logger
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    private var TAG = MainActivity::class.java.simpleName
+    private lateinit var restApi: RestApi
+    private lateinit var log: Logger
+
+
+    @Inject
+    lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        log = Logger.getLogger(TAG)
+
+        setupInjection()
+        fetchDataFromApi()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+    }
+
+    private fun setupInjection(){
+        DaggerViewModelInjector.builder()
+            .build()
+            .inject(this)
+        restApi = retrofit.create(RestApi::class.java)
+
+
+
+    }
+
+    private fun fetchDataFromApi() = GlobalScope.launch {
+        val deferredCharacters = async {
+            restApi.getCharacters("simpsons characters", format).enqueue(object : Callback<CharactersResult> {
+                override fun onFailure(call: Call<CharactersResult>, t: Throwable) {
+                    log.severe("$TAG::onFailure::${t.message}")
+                }
+
+                override fun onResponse(call: Call<CharactersResult>, response: Response<CharactersResult>) {
+                    log.severe("$TAG::onResponse::${response.body()?.results}")
+                }
+            }
+            )
+
+//        deferredCharacters.await()
+//        log.severe("${deferredCharacters.await().body()?.relatedTopics?.size}")
         }
     }
 
@@ -28,9 +81,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
