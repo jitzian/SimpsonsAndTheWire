7eb3074a0c474b069e242e56f8c1109c2ecd38fe
Jonathan Cruz <jitzian@gmail.com>
2019-01-25 17:42:06 -0600

    cc jc - Creating generic injector for DI. Still needs work and some cleanup

---
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/dependency.injection/components/ViewModelInjector.kt
@@ -0,0 +1,21 @@
+package org.com.castcodechallenge.comcastcodechallenge.dependency.injection.components
+
+import dagger.Component
+import org.com.castcodechallenge.comcastcodechallenge.MainActivity
+import org.com.castcodechallenge.comcastcodechallenge.dependency.injection.modules.NetworkModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [NetworkModule::class])
+interface ViewModelInjector {
+
+//    fun inject()
+    fun inject(mainActivity: MainActivity)
+
+    @Component.Builder
+    interface Builder {
+        fun build(): ViewModelInjector
+        fun networkModule(networkModule: NetworkModule): Builder
+    }
+
+}
