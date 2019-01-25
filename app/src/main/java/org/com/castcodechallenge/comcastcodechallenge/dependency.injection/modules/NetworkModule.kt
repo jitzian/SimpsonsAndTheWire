fb24797e69ef7eca5818bb68be49ae9b26c85040
Jonathan Cruz <jitzian@gmail.com>
2019-01-25 17:38:52 -0600

    cc jc - DI Module for Network(Retrofit)

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/dependency.injection/modules/NetworkModule.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/dependency.injection/modules/NetworkModule.kt
@@ -5,16 +5,18 @@ import dagger.Provides
 import dagger.Reusable
 import org.com.castcodechallenge.comcastcodechallenge.constants.GlobalConstants.Companion.baseUrl
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
+@Singleton
 class NetworkModule {
 
     @Provides
     @Reusable
     fun providesRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .baseUrl(baseUrl)
             .build()
     }
