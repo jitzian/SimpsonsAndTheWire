4e5694f7090584c70d2bc2e92a9b8a40d9e7f009
Jonathan Cruz <jitzian@gmail.com>
2019-01-27 12:17:36 -0600

    cc jc - DataClass for saving API instances

---
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/model/Characters.kt
@@ -0,0 +1,13 @@
+package org.com.castcodechallenge.comcastcodechallenge.db.model
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "characters")
+data class Characters(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "url") var url: String? = null,
+    @ColumnInfo(name = "text") var text: String? = null,
+    @ColumnInfo(name = "result") var result: String? = null
+)
