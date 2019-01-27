6b43fec75a4fe6a14cb5a266bd407bef49a9787f
Jonathan Cruz <jitzian@gmail.com>
2019-01-27 12:20:37 -0600

    cc jc - This needs to be removed.
    First I gotta complete the view model logic and then I'll be able to setup the data bindings

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/ui/adapter/RVCharactersAdapter.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/ui/adapter/RVCharactersAdapter.kt
@@ -1,2 +1,29 @@
 package org.com.castcodechallenge.comcastcodechallenge.ui.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import org.com.castcodechallenge.comcastcodechallenge.db.model.Characters
+
+class RVCharactersAdapter(private val context: Context)/*: RecyclerView.Adapter<RVCharactersAdapter.ViewHolder>()*/ {
+    private lateinit var charactersList:List<Characters>
+
+//    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+//
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return if(::charactersList.isInitialized) charactersList.size else 0
+//    }
+//
+//    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+//
+//    }
+//
+//
+//    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder (itemView){
+//
+//    }
+
+}
