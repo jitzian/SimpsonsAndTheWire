988929de3dc370e17f7691949a6ff930a4aea4c2
Jonathan Cruz <jitzian@gmail.com>
2019-01-29 21:55:44 -0500

    cc jc - Renaming DataClass

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/dao/CharactersDao.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/dao/CharactersDao.kt
@@ -4,15 +4,15 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
-import org.com.castcodechallenge.comcastcodechallenge.db.model.Characters
+import org.com.castcodechallenge.comcastcodechallenge.db.model.Character
 
 @Dao
 interface CharactersDao {
 
     @get:Query("SELECT * FROM characters")
-    val getAllCharacters: List<Characters>
+    val getAllCharacters: List<Character>
 
     @Insert(onConflict = REPLACE)
-    fun insert(vararg character: Characters)
+    fun insert(vararg character: Character)
 
 }
