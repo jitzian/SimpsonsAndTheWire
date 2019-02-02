42252f8d8f621de806dfee8c794efa0813c382f1
Jonathan Cruz <jitzian@gmail.com>
2019-02-02 14:15:47 -0500

    cc jc - Adding https to URL

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/constants/GlobalConstants.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/constants/GlobalConstants.kt
@@ -2,7 +2,7 @@ package org.com.castcodechallenge.comcastcodechallenge.constants
 
 class GlobalConstants {
     companion object {
-        const val baseUrl = "http://api.duckduckgo.com"
+        const val baseUrl = "https://api.duckduckgo.com"
         //http://api.duckduckgo.com/?q=simpsons+characters&format=json
         //http://api.duckduckgo.com/?q=the+wire+characters&format=json
         const val format = "json"
@@ -10,7 +10,5 @@ class GlobalConstants {
         //Constants for saving RoomDB
         const val dataBaseName = "comcast.db"
         const val dataBaseVersion = 1
-
-
     }
 }
