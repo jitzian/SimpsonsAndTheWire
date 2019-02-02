a7fb7bb1b1afbb6f299e682d2fbe0516474c672b
Jonathan Cruz <jitzian@gmail.com>
2019-02-02 00:22:45 -0500

    cc jc - Setting binding on AdapterRV

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/ui/adapter/RVCharactersAdapter.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/ui/adapter/RVCharactersAdapter.kt
@@ -1,29 +1,42 @@
 package org.com.castcodechallenge.comcastcodechallenge.ui.adapter
 
-import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import org.com.castcodechallenge.comcastcodechallenge.db.model.Characters
+import org.com.castcodechallenge.comcastcodechallenge.R
+import org.com.castcodechallenge.comcastcodechallenge.databinding.ContentMainBinding
+import org.com.castcodechallenge.comcastcodechallenge.db.model.Character
+import org.com.castcodechallenge.comcastcodechallenge.ui.CharactersViewModel
 
-class RVCharactersAdapter(private val context: Context)/*: RecyclerView.Adapter<RVCharactersAdapter.ViewHolder>()*/ {
-    private lateinit var charactersList:List<Characters>
+class RVCharactersAdapter: RecyclerView.Adapter<RVCharactersAdapter.ViewHolder>() {
+    private lateinit var charactersList:List<Character>
 
-//    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-//
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return if(::charactersList.isInitialized) charactersList.size else 0
-//    }
-//
-//    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-//
-//    }
-//
-//
-//    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder (itemView){
-//
-//    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.content_main, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return if(::charactersList.isInitialized) charactersList.size else 0
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(charactersList[position])
+    }
+
+    fun updateCharacterList(charactersList:List<Character>){
+        this.charactersList = charactersList
+        notifyDataSetChanged()
+    }
+
+
+    inner class ViewHolder(private val binding: ContentMainBinding): RecyclerView.ViewHolder (binding.root){
+        private val viewModel = CharactersViewModel()
+
+        fun bind(character: Character){
+            viewModel.bind(character)
+            binding.viewModel = viewModel
+        }
+    }
 
 }
