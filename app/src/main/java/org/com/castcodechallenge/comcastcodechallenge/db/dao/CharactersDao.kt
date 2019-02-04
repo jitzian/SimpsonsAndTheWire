d196c464ecd43c66e70bde07b9ea1f621043c817
Jonathan Cruz <jitzian@gmail.com>
2019-02-03 22:14:55 -0500

    cc jc - Cleanup of DbDao

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/dao/CharactersDao.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/dao/CharactersDao.kt
@@ -4,16 +4,14 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
+import org.com.castcodechallenge.comcastcodechallenge.constants.selectAllFromCharacters
 import org.com.castcodechallenge.comcastcodechallenge.db.model.Character
 
 @Dao
 interface CharactersDao {
-
-    @get:Query("SELECT * FROM characters")
+    @get:Query(selectAllFromCharacters)
     val getAllCharacters: List<Character>
 
     @Insert(onConflict = REPLACE)
-//    fun insert(vararg character: Character)
     fun insert(character: Character)
-
 }
