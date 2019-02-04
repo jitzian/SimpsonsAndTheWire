9a0bd7a2ab2527764834eeae66dcebb38133fa61
Jonathan Cruz <jitzian@gmail.com>
2019-02-03 22:14:20 -0500

    cc jc - Cleanup ofDatabase

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/CharactersDataBase.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/db/CharactersDataBase.kt
@@ -1,9 +1,7 @@
 package org.com.castcodechallenge.comcastcodechallenge.db
 
 import android.arch.persistence.room.Database
-import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
-import android.content.Context
 import org.com.castcodechallenge.comcastcodechallenge.constants.GlobalConstants
 import org.com.castcodechallenge.comcastcodechallenge.db.dao.CharactersDao
 import org.com.castcodechallenge.comcastcodechallenge.db.model.Character
@@ -14,25 +12,5 @@ import org.com.castcodechallenge.comcastcodechallenge.db.model.Character
     version = GlobalConstants.dataBaseVersion
 )
 abstract class CharactersDataBase : RoomDatabase() {
-
     abstract fun charactersDao(): CharactersDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: CharactersDataBase? = null
-
-        fun getInstance(context: Context): CharactersDataBase? {
-            if (INSTANCE == null) {
-                synchronized(CharactersDataBase::class) {
-                    INSTANCE = Room.databaseBuilder(
-                        context.applicationContext,
-                        CharactersDataBase::class.java, GlobalConstants.dataBaseName
-                    ).fallbackToDestructiveMigration().build()
-                }
-            }
-            return INSTANCE
-        }
-
-    }
-
 }
