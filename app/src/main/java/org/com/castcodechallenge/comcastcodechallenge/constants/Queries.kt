d8815de543aeb5d82a0f5d8c409330e3ae17713d
Jonathan Cruz <jitzian@gmail.com>
2019-02-04 22:21:16 -0500

    cc jc - Adding query param by different characters

--- app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/constants/Queries.kt
+++ app/src/main/java/org/com/castcodechallenge/comcastcodechallenge/constants/Queries.kt
@@ -1,3 +1,5 @@
 package org.com.castcodechallenge.comcastcodechallenge.constants
 
 const val selectAllFromCharacters = "SELECT * FROM characters"
+const val simpsonsQuery = "simpsons characters"
+const val theWireQuery = "the wire characters"
